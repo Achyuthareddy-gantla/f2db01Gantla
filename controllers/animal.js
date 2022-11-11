633bdd6ed7c518e3176c1e0c9514612d5a1392b5
Achyutha Reddy <s550301@nwmissouri.edu>
2022-11-11 11:23:23 -0600

    modified animal.js in controllers

--- controllers/animal.js
+++ controllers/animal.js
@@ -1,5 +1,5 @@
 var Animal = require('../models/animal');
-// List of all Costumes
+// List of all Animals
 exports.animal_list = async function(req, res) {
     try{
     theAnimals = await Animal.find();
@@ -11,17 +11,24 @@ exports.animal_list = async function(req, res) {
     }
    };
 // for a specific animal.
-exports.animal_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: animal detail: ' + req.params.id);
-};
-// Handle Costume create on POST.
+exports.animal_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Animal.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+// Handle Animal create on POST.
 exports.animal_create_post = async function(req, res) {
     console.log(req.body)
     let document = new Animal();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"Animal_type":"goat", "cost":12, "size":"large"}
     document.animalName = req.body.animalName;
     document.animalWeight= req.body.animalWeight;
     document.animalType = req.body.animalType;
@@ -39,9 +46,25 @@ exports.animal_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: animal delete DELETE ' + req.params.id);
 };
 // Handle animal update form on PUT.
-exports.animal_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: animal update PUT' + req.params.id);
-};
+exports.animal_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await Animal.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.animalName)
+    toUpdate.animalName = req.body.animalName;
+    if(req.body.animalWeight) toUpdate.animalWeight = req.body.animalWeight;
+    if(req.body.animalType) toUpdate.animalType = req.body.animalType;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };
 
 // VIEWS
 // Handle a show all view
