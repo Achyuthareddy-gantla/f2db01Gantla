8358d6c6a2485295f8ee989730c5affe298e389b
Achyutha Reddy <s550301@nwmissouri.edu>
2022-11-30 01:07:50 -0600

    made changes in login, register pug

--- app.js
+++ app.js
@@ -3,8 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+))
 
 require('dotenv').config();
 const connectionString =
@@ -16,7 +33,7 @@ mongoose.connect(connectionString,
     useUnifiedTopology: true
   });
 
-//Get the default connection
+  //Get the default connection
 var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -24,18 +41,75 @@ db.once("open", function () {
   console.log("Connection to DB succeeded")
 });
 
+
 var Animal = require("./models/animal");
 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var resourceRouter = require('./routes/resource');
 var animalRouter = require('./routes/animal');
 var gridbuildRouter = require('./routes/gridbuild');
 var selector = require('./routes/selector');
+var resourceRouter = require('./routes/resource');
+
 const { use } = require('./routes/animal');
 
 var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/animal', animalRouter);
+app.use('/gridbuild', gridbuildRouter)
+app.use('/selector', selector)
+app.use('/resource', resourceRouter)
+
+
+
+
+// passport config
+// use the existing connection
+// the account model
+var Account = require('./models/account');
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 // We can seed the collection if needed on server start
 async function recreateDB() {
@@ -46,12 +120,12 @@ async function recreateDB() {
       animalName: "rabbit", animalWeight: 50,
       animalType: "Domestic"
     });
-    let instance2 = new
+  let instance2 = new
     Animal({
       animalName: "Lion", animalWeight: 500,
       animalType: "Wild"
     });
-    let instance3 = new
+  let instance3 = new
     Animal({
       animalName: "Elephant", animalWeight: 700,
       animalType: "Wild"
@@ -69,55 +143,8 @@ async function recreateDB() {
     console.log("Third object saved")
   });
 }
+
 let reseed = true;
 if (reseed) { recreateDB(); }
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/animal', animalRouter);
-app.use('/gridbuild', gridbuildRouter)
-app.use('/selector', selector)
-
-app.use(require('express-session')({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false
- }));
- app.use(passport.initialize());
- app.use(passport.session());
- app.use('/resource',resourceRouter)
- 
- app.use(require('express-session')({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false
- }));
- app.use(passport.initialize());
- app.use(passport.session());
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
--- models/animal.js
+++ models/animal.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose")
 const animalSchema = mongoose.Schema({
-animalName: String,
-animalWeight: Number,
-animalType: String
-})
+    animalName: {
+        type: String,
+        required: [true, 'Name of the Animal cannot be empty']
+    },
+    animalWeight: {
+        type: Number,
+        required: [true, 'Weight of the Animal cannot be empty']
+    },
+    animalType: {
+        type: String,
+        required: [true, 'Animal Type cannot be empty']
+    },
+
+});
 module.exports = mongoose.model("Animal",
 animalSchema)
